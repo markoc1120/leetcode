c8bbaa0455aa211658da1b34c9ae2d6236e1802c
markoc1120 <markoc1120@gmail.com>
2024-04-03 17:52:45 +0200

    930. Binary Subarrays With Sum - sliding window solution

--- python/binary-subarrays-with-sum.py
+++ python/binary-subarrays-with-sum.py
@@ -15,3 +15,23 @@ def numSubarraysWithSum(nums, goal):
             ans += prefix_sum[curr - goal]
         prefix_sum[curr] += 1
     return ans
+
+
+# Time complexity O(1)
+# Space complexity O(n)
+def numSubarraysWithSum(nums, goal):
+    prefix_zeros = 0
+    left = curr = ans = 0
+    for right, num in enumerate(nums):
+        curr += num
+        while left < right and (nums[left] == 0 or curr > goal):
+            if nums[left] == 1:
+                prefix_zeros = 0
+            else:
+                prefix_zeros += 1
+
+            curr -= nums[left]
+            left += 1
+        if curr == goal:
+            ans += 1 + prefix_zeros
+    return ans
