4f5ebdae65f69858aa5c2c5a5a71fa5227805159
markoc1120 <markoc1120@gmail.com>
2026-03-01 13:12:30 +0100

    1047. Remove All Adjacent Duplicates In String

---
+++ python/remove-all-adjacent-duplicates-in-string.py
@@ -0,0 +1,10 @@
+# Time complexity O(n)
+# Space complexity O(n)
+def removeDuplicates(s: str) -> str:
+    stack = []
+    for ch in s:
+        if stack and stack[-1] == ch:
+            stack.pop()
+        else:
+            stack.append(ch)
+    return ''.join(stack)
